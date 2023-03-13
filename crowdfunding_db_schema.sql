eca088797f4b2407cb05862c06eb69fb88cae893
TroyFoote <troyfoote@gmail.com>
2023-03-13 18:05:43 +0800

    created crowdfunding schema and ERD

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "CAMPAIGN" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" INTEGER   NOT NULL,
+    "pledged" INTEGER   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_CAMPAIGN" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "CATEGORY" (
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_CATEGORY" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "SUBCATEGORY" (
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_SUBCATEGORY" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "CONTACTS" (
+    "contact_id" INTEGER   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_CONTACTS" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "CONTACTS" ("contact_id");
+
+ALTER TABLE "CATEGORY" ADD CONSTRAINT "fk_CATEGORY_category_id" FOREIGN KEY("category_id")
+REFERENCES "CAMPAIGN" ("category_id");
+
+ALTER TABLE "SUBCATEGORY" ADD CONSTRAINT "fk_SUBCATEGORY_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "CAMPAIGN" ("subcategory_id");
+
